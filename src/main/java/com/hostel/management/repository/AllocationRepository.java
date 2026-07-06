43229a29b6c02a07ab87db2e72e4440b6f991170
ruqiyoabdirashid <rawanabdi83@gmail.com>
2026-07-06 01:58:23 -0700

    Added AllocationRepository

---
+++ src/main/java/com/hostel/management/repository/AllocationRepository.java
@@ -0,0 +1,16 @@
+package com.hostel.management.repository;
+
+import com.hostel.management.entity.Allocation;
+import com.hostel.management.entity.enums.AllocationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+
+    List<Allocation> findByStudentId(Long studentId);
+
+    List<Allocation> findByRoomId(Long roomId);
+
+    List<Allocation> findByStatus(AllocationStatus status);
+}
